e79771f619e8c8ffa6c85f8a740a7e8a24561d47
ahephner <ajhephner178@gmail.com>
2021-06-23 15:29:27 -0400

    starting on adding lwc to replace mobile native sf lookup

---
+++ force-app/main/default/lwc/mobileProdLookUp/mobileProdLookUp.js
@@ -0,0 +1,7 @@
+import { LightningElement } from 'lwc';
+import searchProd from '@salesforce/apex/lookUpFlow.searchProd';
+
+const SEARCH_DELAY = 500; 
+export default class MobileProdLookUp extends LightningElement {
+
+}
