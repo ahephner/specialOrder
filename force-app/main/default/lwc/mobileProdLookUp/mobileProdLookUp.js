881a7cf534692b5d834e0ac401ac3fa23de507ca
ahephner <ajhephner178@gmail.com>
2021-06-24 09:45:05 -0400

    qty and description good. Starting point to wire search

--- force-app/main/default/lwc/mobileProdLookUp/mobileProdLookUp.js
+++ force-app/main/default/lwc/mobileProdLookUp/mobileProdLookUp.js
@@ -1,7 +1,44 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
+import { FlowAttributeChangeEvent} from 'lightning/flowSupport';
 import searchProd from '@salesforce/apex/lookUpFlow.searchProd';
 
 const SEARCH_DELAY = 500; 
 export default class MobileProdLookUp extends LightningElement {
+    //vars to flow
+    @api prodDesc;
+    @api qty; 
+    @api prodId; 
+    loading = false; 
+    //search varss
+    minSearchTerm = 3;
+    queryTerm;
+    searchTimeOut;
+    productName;
+    prodsId;
+    showResult = false; 
+    
+    handleQty(e){
+        this.qty = e.detail.value;
 
+        const attributeChange = new FlowAttributeChangeEvent('qty', this.qty);
+        this.dispatchEvent(attributeChange); 
+    }
+
+    newDescription(e){
+        window.clearTimeout(this.delayTimeout);
+        // eslint-disable-next-line @lwc/lwc/no-async-operation
+        this.delayTimeout = setTimeout(()=>{
+            this.prodDesc = n.detail.value; 
+            const attributeChange = new FlowAttributeChangeEvent('prodDesc', this.prodDesc);
+             this.dispatchEvent(attributeChange); 
+        },400); 
+    }
+//search for product
+
+
+
+//drop down class
+    get getListBoxClass(){
+        return 'slds-listbox slds-listbox_vertical slds-dropdown slds-dropdown_fluid';
+    }
 }
