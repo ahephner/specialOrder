10998e114344cfc0454b53aecf1322a5a834d885
ahephner <ajhephner178@gmail.com>
2021-07-26 15:51:39 -0400

    added ability to delete rows from table. Added button to add new products

--- force-app/main/default/lwc/specialOrderProductMP/specialOrderProductMP.js
+++ force-app/main/default/lwc/specialOrderProductMP/specialOrderProductMP.js
@@ -2,9 +2,14 @@ import { LightningElement, api, wire, track } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import FORM_FACTOR from '@salesforce/client/formFactor';
 import getItems from '@salesforce/apex/lookUpFlow.getOrderRequestItems';
-import { updateRecord } from 'lightning/uiRecordApi';
+import { updateRecord, deleteRecord } from 'lightning/uiRecordApi';
 import { refreshApex } from '@salesforce/apex';
+import { NavigationMixin } from 'lightning/navigation';
+import { encodeDefaultFieldValues } from 'lightning/pageReferenceUtils';
 
+const actions = [
+    {label: 'Delete', name:'delete'},
+]
 const columns = [
     {label:'Product Requested', 'fieldName':'nameURL', type:'url', typeAttributes:{label:{fieldName:'product'}},target:'_blank' },
     {label:'QTY', 'fieldName':'Quantity_Requested__c', type:'number', cellAttributes:{alignment: 'center'} },
@@ -12,9 +17,13 @@ const columns = [
     {label:'Min Margin', 'fieldName':'Minimum_Margin__c', type:'percent-fixed', editable:true, cellAttributes:{alignment: 'center'} },
     {label:'Sales Margin', 'fieldName':'Sales_Margin__c', type:'percent-fixed',  editable:true, cellAttributes:{alignment: 'center'}},
     {label:'Unit Price', 'fieldName':'Unit_Price__c', type:'currency', editable:true, cellAttributes:{alignment: 'center'}},
+    {
+        type:'action',
+        typeAttributes: {rowActions:actions}
+    }
 ]
 
-export default class SpecialOrderProductMP extends LightningElement {
+export default class SpecialOrderProductMP extends NavigationMixin(LightningElement) {
     isLoading; 
     @api recordId;
     //make component aware of size
@@ -170,7 +179,60 @@ export default class SpecialOrderProductMP extends LightningElement {
 
             this.tableUpdate(tempId, tempType, value);
         }
+///Add new product to table
+        addProduct(){
+            const setRec = encodeDefaultFieldValues({
+                Order_Request__c: this.recordId
+            })
+            
+            this[NavigationMixin.Navigate]({
+                type:'standard__objectPage',
+                attributes: {
+                    objectApiName: 'Order_Request_Detail__c',
+                    actionName: 'new'
+                },
+                state: {
+                    defaultFieldValues: setRec
+                }
 
+            });
+        }
+//Delete from the table
+        handleAction(i){
+            const actionName = i.detail.action.name;
+            const row = i.detail.row.Id;
+            //console.log('row '+row);
+            
+            switch (actionName) {
+                case 'delete':{
+                        // eslint-disable-next-line no-case-declarations
+                        // eslint-disable-next-line no-alert
+                        let cf = confirm('Do you want to delete this entry?')
+                        if(cf===true){
+                    deleteRecord(row)
+                        .then(() => {
+                            this.dispatchEvent(
+                                new ShowToastEvent({
+                                    title: 'Success', 
+                                    message: 'Product Deleted', 
+                                    variant: 'success'
+                                }) 
+                            );//this refreshes the table  
+                            return refreshApex(this.requestItems)
+                        })
+                        .catch(error => {
+                            this.dispatchEvent(
+                                new ShowToastEvent({
+                                    title: 'Error deleting record',
+                                    message: JSON.stringify(error),
+                                    variant: 'error'
+                                })
+                            )
+                        })
+                    }
+                }
+            }
+        }
 //Mobile stuff//////
         handleMargin(m){
             let index = this.items.findIndex(prod => prod.Id === m.target.name);
